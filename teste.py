2d20e69b6922331c8d5c2530d5b4477974de76ca
Perdigueira <mateussilvaaugusto@gmail.com>
2025-08-04 18:50:59 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,33 @@
+operacao = input("Digite a operação: ")
+
+if operacao == "+":
+
+    valor1 = int(input("Digite o primeiro valor: "))
+    valor2 = int(input("Digite o segundo valor: "))
+
+    print(f'{valor1} + {valor2} = {valor1 + valor2}')
+
+elif operacao == "-":
+
+    valor1 = int(input("Digite o primeiro valor: "))
+    valor2 = int(input("Digite o segundo valor: "))
+
+    print(f'{valor1} - {valor2} = {valor1 - valor2}')
+
+elif operacao == "*":
+
+    valor1 = int(input("Digite o primeiro valor: "))
+    valor2 = int(input("Digite o segundo valor: "))
+
+    print(f'{valor1} * {valor2} = {valor1 * valor2}')
+
+elif operacao == "/":
+
+    valor1 = float(input("Digite o primeiro valor: "))
+    valor2 = float(input("Digite o segundo valor: "))
+
+    print(f'{valor1} / {valor2} = {valor1 / valor2}')
+
+else:
+    print("fim")
+
